c61b2396576ef4e781497833a57738b0e2c132a8
allan <allansaleh95@gmail.com>
2020-04-20 11:48:29 +0300

    challenge 1 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+
+document.addEventListener('DOMContentLoaded', function() {
+	fetch(imgUrl).then((respond) => respond.json()).then((results) => {
+		results.message.forEach((image) => addImage(image));
+	});
+});
+
+function addImage(imgUrl) {
+	let container = document.querySelector('#dog-image-container');
+	let img = document.createElement('img');
+	img.setAttribute('src', imgUrl);
+	container.appendChild(img);
+}
