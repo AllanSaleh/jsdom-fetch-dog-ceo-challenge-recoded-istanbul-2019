49e6b3bacbe969325a7fdfd6d2ffb7def5f61dff
allan <allansaleh95@gmail.com>
2020-04-20 11:52:35 +0300

    cleaned code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ console.log('%c HI', 'color: firebrick');
 const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
 
 document.addEventListener('DOMContentLoaded', function() {
-	fetch(imgUrl).then((respond) => respond.json()).then((results) => {
+	fetchImage(imgUrl);
+});
+
+function fetchImage(url) {
+	fetch(url).then((respond) => respond.json()).then((results) => {
 		results.message.forEach((image) => addImage(image));
 	});
-});
+}
 
 function addImage(imgUrl) {
 	let container = document.querySelector('#dog-image-container');
